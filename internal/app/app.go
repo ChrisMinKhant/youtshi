881e5f300f7f37607a21b54d0c997ff75e99a945
Kaung Min Khant <kaungminkhant05723123@gmail.com>
2023-11-18 21:19:39 +0630

    added local env

--- internal/app/app.go
+++ internal/app/app.go
@@ -4,12 +4,20 @@ import (
 	"log"
 	"net/http"
 	"v1/internal/controller"
+	"v1/util"
 )
 
 func Init() {
-	log.Printf("Starting the server and listening at port >>> %s", "8080")
+	serverPort := ":8080"
+
 	http.HandleFunc("/", controller.HandleTest)
 	http.HandleFunc("/sec", controller.HandleSecondTest)
 
-	http.ListenAndServe(":8080", nil)
+	if util.GetEvnValue("SERVER_PORT") != "" {
+		log.Println(" Custom server port was found! ")
+		serverPort = util.GetEvnValue("SERVER_PORT")
+	}
+
+	log.Printf("Starting the server and listening at port >>> %s", serverPort)
+	http.ListenAndServe(serverPort, nil)
 }
--- util/environment.go
+++ util/environment.go
@@ -1,7 +1,6 @@
 package util
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/joho/godotenv"
@@ -16,23 +15,14 @@ func GetEvnValue(key string) string {
 }
 
 func getEnvMap() map[string]string {
-	defer func() {
-		r := recover()
-
-		if r != nil {
-			fmt.Printf("Recover status >>> %s \n", "true")
-		}
-	}()
 
 	var envMap map[string]string
 
 	envMap, err := godotenv.Read(".env")
 
 	if err != nil {
-		log.Panicf("Found error at getting environment map >> %s", err)
+		log.Fatalf("Found error at getting environment map >> %s", err)
 	}
-
-	defer log.Println("Defer after panic!")
 
 	return envMap
 }
